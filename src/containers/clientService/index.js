ca0af0369665a7edd3627587b5091906ec41bcca
zuoguangcheng <zuoguangcheng@shuidao.com>
2018-01-22 20:00:40 +0800

    appjs

--- src/containers/clientService/index.js
+++ src/containers/clientService/index.js
@@ -1,7 +1,7 @@
 import * as io from 'socket.io-client';
 import Context from './context';
 
-let url = 'http://chat.zuoguangcheng.top:3000';
+let url = 'http://39.106.118.108:3000';
 let cxt = null;
 
 export function getCxt() {
